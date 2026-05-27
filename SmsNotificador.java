aecc6e7aedd4f4ddd84678bb1409741d280bd3ec
Miguel-JW <conteudojurassicojw@gmail.com>
2026-05-27 09:12:14 -0300

    Create SmsNotificador.java

---
+++ SmsNotificador.java
@@ -0,0 +1,7 @@
+public class SmsNotificador implements Notificador {
+
+    @Override
+    public void enviarMensagem(String mensagem) {
+        System.out.println("📱 [SMS] Mensagem enviada: " + mensagem);
+    }
+}
